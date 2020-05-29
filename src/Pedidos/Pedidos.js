a0c960e9f110e1cc498d2af8b86dd6b843d2630d
Giuliano Crespe <gcrespe3@hotmail.com>
2020-05-29 14:23:06 -0300

    alerta adiciondo

--- src/Pedidos/Pedidos.js
+++ src/Pedidos/Pedidos.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
 
 const Pedidos = (props) => {
 
+    const [show, setShow] = useState(false);
+
     let contador = 0; 
     let desconto = 0;
+    
     function calculaDesconto(){
         if(props.quantGOT){
             contador = contador + 1;
@@ -51,31 +56,55 @@ const Pedidos = (props) => {
     }
     return(
         <Container style={{ width: '100vh'}}>
-            <Row sm="12"> 
-                <Col md="12" sm="3">
+            <Row sm="6"> 
+                <Col md="12" sm="12">
+                <Alert show={show} variant="success">
+                    <Alert.Heading>Pedido Finalizado</Alert.Heading>
+                        <p>
+                            Seu pedido foi contabilizado e irá partir para a entrega após a postagem
+                        </p>
+                        <hr />
+                    <div className="d-flex justify-content-end">
+                    <Button onClick={() => setShow(false)} variant="outline-success">
+                        Fechar
+                    </Button>
+                    </div>
+                </Alert>
                     <Card>
-                        <Card.Header>Pedido #{props.id+1}</Card.Header>
+                        <Card.Header>Pedido #1</Card.Header>
                         <Card.Body>
                             <blockquote className="blockquote mb-0">
-                            <p>
-                                <p>
-                                    Game of Thrones: {props.quantGOT}
-                                </p>
-                                <p>
-                                    Jogos Vorazes: {props.quantJV}
-                                </p>
-                                <p>
-                                    Percy Jackson: {props.quantPJ}
-                                </p>
-                                <p>
-                                    Harry Potter: {props.quantHP}
-                                </p>
-                                <p>
-                                    Coleçao Nietzsche: {props.quantNT}
-                                </p>
-                                <p>
-                                    Desconto: {calculaDesconto()*100}%
-                                </p>
+                            <p style={{marginLeft: '20px'}}>
+                                <Row sm="12">
+                                    <p>
+                                        Game of Thrones: {props.quantGOT}
+                                    </p>
+                                </Row>
+                                <Row sm="12">
+                                    <p>
+                                        Jogos Vorazes: {props.quantJV}
+                                    </p>
+                                </Row>
+                                <Row sm="12">
+                                    <p>
+                                        Percy Jackson: {props.quantPJ}
+                                    </p>
+                                </Row> 
+                                <Row sm="12">
+                                    <p>
+                                        Harry Potter: {props.quantHP}
+                                    </p>
+                                </Row>
+                                <Row sm="12">
+                                    <p>
+                                        Coleçao Nietzsche: {props.quantNT}
+                                    </p>
+                                </Row>
+                                <Row sm="12"    >
+                                    <p>
+                                        Desconto: {calculaDesconto()*100}%
+                                    </p>
+                                </Row>
                             </p>
                             <footer className="blockquote-footer">
                                 Preço total:  <cite title="Source Title">R$ {calculaPrecoTotal()} </cite>
@@ -85,9 +114,9 @@ const Pedidos = (props) => {
                     </Card>
                     <Row className="justify-content-md-center"> 
                         <Col md="auto" >
-                            <button className='button1' style={{marginTop: '50px'}}>
+                            {!show && <button className='button1' style={{marginTop: '50px'}} onClick={() => setShow(true)}>
                                 FINALIZAR
-                            </button>
+                            </button>}
                         </Col>
                     </Row>
                 </Col>
